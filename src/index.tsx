6893785b2dd29b9c3e2fb316555fc432a1258a37
Annie Huang <annie.hn@gmail.com>
2022-05-11 22:09:31 +1000

    Update src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { Provider } from 'react-redux';
@@ -12,9 +11,7 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
