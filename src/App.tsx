07666c5af50e51ccafdcae93922f80052fd332f4
Annie Huang <annie.hn@gmail.com>
2022-05-11 22:14:29 +1000

    Advance example with pollingInterval

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/*
 import './styles.css';
 import { useGetPokemonByNameQuery } from './services/pokemon';
 
@@ -19,6 +20,35 @@ export default function App() {
           <img src={data.sprites.front_shiny} alt={data.species.name} />
         </>
       ) : null}
+    </div>
+  );
+}
+*/
+
+// Advance example
+import './styles.css';
+import { Pokemon } from './Pokemon';
+import { useState } from 'react';
+
+const pokemon = ['bulbasaur', 'pikachu', 'ditto', 'bulbasaur'];
+
+export default function App() {
+  const [pollingInterval, setPollingInterval] = useState(0);
+
+  return (
+    <div className='App'>
+      <select
+        onChange={(change) => setPollingInterval(Number(change.target.value))}
+      >
+        <option value={0}>Off</option>
+        <option value={1000}>1s</option>
+        <option value={5000}>5s</option>
+      </select>
+      <div>
+        {pokemon.map((poke, index) => (
+          <Pokemon key={index} name={poke} pollingInterval={pollingInterval} />
+        ))}
+      </div>
     </div>
   );
 }
